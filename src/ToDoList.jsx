aefe8ed13ea0dbd774b63b5ecc1bb3f77909f628
Flavio <flaviofilholm10@gmail.com>
2023-12-08 16:15:18 -0300

    updating

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import "./ToDoList.css";
 import { Card } from "./components/Card"
+import { v4 as uuidv4 } from 'uuid'
 
 // my ToDoList function to be exported to index.jsx
 export function ToDoList() {
@@ -19,20 +20,47 @@ export function ToDoList() {
     }
 
     const newTask = {
-      id: 0,
+      id: uuidv4(),
       text: task,
+      completed: false,
     }
 
     setTask(prevState => [...prevState, newTask]);
     document.querySelector("#inTask").value = "";
   }
 
-  function editTask() {
-    console.log("Bt clicked");
+  const editTask = (taskId) => {
+    const modal = document.querySelector("#modal");
+    const closeModal = document.querySelector("#close-modal");
+    const btEdit = document.querySelectorAll(".btEdit");
+
+    Array.from(btEdit).forEach(edit => {
+      edit.addEventListener("click", () => modal.style.display = "inline")
+    })
+
+    closeModal.addEventListener("click", () => {
+      modal.style.display = "none";
+    })
+    const inTaskEdit = document.querySelector("#inTextEdit");
+    const task = tasks.find(task => task.id === taskId);
+    inTaskEdit.value = task.text;
+  }
+
+  function handleTask(taskId) {
+    // const newTasks = tasks.map((task) => {
+    //   if (task.id === taskId) {
+    //     return { ...tasks, completed: !task.completed }
+    //   }
+
+    //   return task;
+    // });
+    // setTask(newTasks);
   }
 
   function deleteTask() {
-    console.log("Bt Delete clicked");
+    if (window.confirm("Are you sure to delete this task: ")) {
+      console.log("Task deleted")
+    };
   }
 
   return (
@@ -42,20 +70,33 @@ export function ToDoList() {
           <h1>To Do List</h1>
         </div>
         <div className="box-container">
-          <input id="inTask" type="text" placeholder="Write your task" className="input" />
+          <input id="inTask" type="text" placeholder="Write your task" className="input" maxLength={60} />
           <button className="button" onClick={addTask}>Add Task</button>
         </div>
+        <div className="modal" id="modal">
+          <div className="box-modal">
+            <div className="header-modal">
+              <h1>Edit your task</h1>
+              <img src={"x-white.png"} alt="" id="close-modal" onClick={editTask} />
+            </div>
+            <input type="text" id="inTextEdit" />
+            <button className="button">Save</button>
+          </div>
+        </div>
         <div className="box-content">
-          {tasks.map((item, index) =>
+          {tasks.map((item) =>
             <Card
-              id={index++}
+              id={item.id}
               text={item.text}
+              completed={item.completed}
               editTask={editTask}
               deleteTask={deleteTask}
+              handleTask={handleTask}
             />
           )}
         </div>
       </div>
+
     </>
   );
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,16 @@
 import "./Card.css"
 
-export function Card({text, editTask, deleteTask}) {
+export function Card({ id, text, completed, editTask, deleteTask, handleTask }) {
     return (
         <>
-            <div className="card">
+            <div
+                className={`card ${completed ? "card-status" : ""}`}
+                id={id}
+                onClick={() => handleTask(id)}
+            >
                 <p>{text}</p>
                 <div className="imgs">
-                    <img className="btEdit" src={"edit.png"} alt="" onClick={editTask} />
+                    <img className="btEdit" src={"edit.png"} alt="" onClick={() => editTask(id)} />
                     <img className="btDelete" src={"x.png"} alt="" onClick={deleteTask} />
                 </div>
             </div>
